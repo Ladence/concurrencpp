6430a52c8ede2154a1355e8132132cbfd90b4b4a
unknown <dhayim1@gmail.com>
2021-03-19 12:44:02 +0200

    ..

--- test/source/tests/dummy_test.cpp
+++ test/source/tests/dummy_test.cpp
@@ -8,16 +8,20 @@
 using namespace concurrencpp::tests;
 
 int main(){
-	object_observer observer;	
-	concurrencpp::runtime runtime;
+	const auto worker_count = thread::hardware_concurrency();
+    const auto task_count = worker_count * 10'000;
+
+    object_observer observer;
+    auto executor = std::make_shared<thread_pool_executor>("threadpool", worker_count, std::chrono::seconds(10));
+    
+    for (size_t i = 0; i < task_count; i++) {
+        executor->post(observer.get_testing_stub());
+    }
+
+    assert_true(observer.wait_execution_count(task_count, std::chrono::minutes(2)));
+    assert_true(observer.wait_destruction_count(task_count, std::chrono::minutes(2)));
 	
-	for(size_t i = 0; i< 30000 ; i++){
-		runtime.thread_pool_executor()->post([stub = observer.get_testing_stub()] () mutable {
-			stub();	
-		});
-	}
-	
-	std::this_thread::sleep_for(std::chrono::seconds(20));
+	executor->shutdown();
 	
 	return 0;
 }
